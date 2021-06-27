85a734266898c955e13e21ae05a0db06e8ecb437
AnaCarolinaFortunato <anacarolinafortunatosantos@gmail.com>
2021-06-26 21:01:40 -0300

    Adicionar arquivos de projeto.

---
+++ AppCurso/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using AppCurso.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppCurso.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    
+    {
+        [HttpPost]
+        [Route("logar")]
+        public IActionResult Logar (LoginViewModelInput LoginViewModelInput)
+        {
+            return Ok (LoginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public IActionResult Registrar(RegistroViewModelInput RegistroViewModelInput)
+        {
+            return Created("", RegistroViewModelInput);
+        }
+
+
+    }
+}
